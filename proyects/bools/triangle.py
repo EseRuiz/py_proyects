34c8dc587977154b93b67774853e5f511fb7c03e
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2023-12-20 13:45:17 -0500

    se agregan ejercicios boleanos

---
+++ proyects/bools/triangle.py
@@ -0,0 +1,26 @@
+def equilateral(sides):
+    if 0 in sides:
+        return False
+    if len(set(sides)) == 1:
+        return True
+    return False
+
+def isosceles(sides):
+    if 0 in sides:
+        return False
+    a,b,c= sorted(sides)
+    if len(set(sides))==2 and a+1==b or a-1==b or len(set(sides)) == 1 or len(set(sides))==2 and a<b or a>b :
+        return True
+    return False
+
+
+def scalene(sides):
+    if 0 in sides or len(set(sides))==2 or len(set(sides)) == 1:
+        return False
+    a,b,c=sorted(sides)
+    if a+b > c and a+c > b and b+c >a:
+        return True
+    return False
+        
+a=isosceles([4, 4, 4])
+print(a)
