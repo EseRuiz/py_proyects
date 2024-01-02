ff583a3c729a6f6cc58775f8a42af3a5b665552c
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2024-01-02 18:07:45 -0500

    prueba de lista de comprension

---
+++ proyects/list_compre.py
@@ -0,0 +1,28 @@
+
+cuadrados = [(int(a)**2) for a in range (1,101) ]
+pares = [ par for par in range(1,21) if par % 2 == 0]
+impares = [impar for impar in range (1,21) if impar % 2 != 0]
+a = [uno for uno in range (1,8)]
+b = [dos for dos in range (3,11)]
+solo_lista = [ una for una in a if una in b]
+solo_lista_dif = [ una for una in a if una not in b]
+
+
+sta = "hello"
+stb = "world"
+
+strin_list = [ st for st in sta if st in stb]
+
+impares_cuad = [(int(im)**2) for im in range (1,101) if im % 2 != 0]
+
+set_list = {num for num in range (1,11) if  num % 3 == 0}
+
+tuples = [('a', 1), ('b', 2), ('c', 3)]
+
+dict_tuple = {llave : valor for llave,valor in tuples if llave == 'b' }
+
+name  = 'example'
+
+dict_word = { letra : name.count(letra) for letra in name}
+
+print(dict_word)
