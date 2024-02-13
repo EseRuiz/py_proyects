23e88bb5f83fc8320a9a851f5f5ad6cd8183b9e5
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2024-02-12 19:22:42 -0500

    list, dinamyc programattion, list comprehetion for dictionaries, tuples uses

--- proyects/list_compre.py
+++ proyects/list_compre.py
@@ -30,4 +30,100 @@ name  = 'example'
 
 dict_word = { letra : name.count(letra) for letra in name}
 
-print(dict_word)
+var = [
+    {      
+      "name": "tipo_patente",
+      "type": "int",
+      "view": "select",
+      "label": "Tipo de patente",
+      "required": False,
+      "included": True,
+      "configurable": False,
+      "readonly": False,
+      "editable": False,},
+    {
+      "name": "tipo_patente2",
+      "type": "int",
+      "view": "select",
+      "label": "Tipo de patente",
+      "required": False,
+      "included": True,
+      "configurable": False,
+      "readonly": False,
+      "editable": False,
+    },
+    {
+      "name": "tipo_patente3",
+      "type": "int",
+      "view": "select",
+      "label": "Tipo de patente",
+      "required": False,
+      "included": True,
+      "configurable": False,
+      "readonly": False,
+      "editable": True,
+    }
+
+]
+new = [les for les in var if les["editable"]==True]
+
+
+
+
+
+
+
+prueba =[
+    {
+     "name":"papa",
+     "apellido":"pastusa",
+     "ensalada":False
+    },
+    {
+     "name":"papa",
+     "apellido":"sabanera",
+     "ensalada":True
+    },
+    {
+     "name":"papa",
+     "apellido":"criolla",
+     "ensalada":True
+    },
+    {
+     "name":"papa",
+     "apellido":"ibia",
+     "ensalada":False
+    },
+    {
+     "name":"papa",
+     "apellido":"francesa",
+     "ensalada":True
+    }
+]
+
+list1 = [var for var in prueba if var["ensalada"]==True]
+list2 = [var for var in prueba if var["ensalada"]==False]
+
+
+def max_sum_no_adjacent(nums):
+    if not nums:
+        return 0
+
+    if len(nums) <= 2:
+        return max(nums)
+
+    dp = [0] * len(nums)
+    print([0] * len(nums))
+    dp[0] = nums[0]
+    print(dp[0] )
+    dp[1] = max(nums[0], nums[1])
+    print( max(nums[0], nums[1]))
+
+    for i in range(2, len(nums)):
+        print(len(nums))
+        dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+        print(dp[i])
+
+    return dp[-1]
+lista = [6, 2, 5, 94, 1, 7]
+print(max_sum_no_adjacent(lista))
