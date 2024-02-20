ab7e288864d3b5fad5f0a4905f4b200019f11029
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2024-02-20 08:13:12 -0500

    fix

--- proyects/clases/clases.py
+++ proyects/clases/clases.py
@@ -1,23 +1,4 @@
-"""Solution to Ellen's Alien Game exercise."""
-
-
 class Alien:
-    """Create an Alien object with location x_coordinate and y_coordinate.
-
-    Attributes
-    ----------
-    (class)total_aliens_created: int
-    x_coordinate: int - Position on the x-axis.
-    y_coordinate: int - Position on the y-axis.
-    health: int - Number of health points.
-
-    Methods
-    -------
-    hit(): Decrement Alien health by one point.
-    is_alive(): Return a boolean for if Alien is alive (if health is > 0).
-    teleport(new_x_coordinate, new_y_coordinate): Move Alien object to new coordinates.
-    collision_detection(other): Implementation TBD.
-    """
 
     health = 3
     total_aliens_created = 0
