b04cd4215e0c66882cdf74abcfb17607463d2f8c
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2024-04-25 08:23:50 -0500

    Fix: <se corrige squares of numbers con formula>

--- proyects/lists/difference_of_squares.py
+++ proyects/lists/difference_of_squares.py
@@ -1,13 +1,13 @@
 def square_of_sum(number):
-    val = [num for  num in range(1,number+1)]
+    #val = [num for  num in range(1,number+1)]
+    val = ((number * (number + 1)) / 2) **2
     
-    return sum(val)**2
-
+    return (int(val))
 
 def sum_of_squares(number):
-    val = [num**2 for num in range(1,number+1)]
-    return sum(val)
-
+    #val = [num**2 for num in range(1,number+1)]
+    val = (number * (number + 1) * (2 * number + 1)) / 6
+    return (int(val))
 
 def difference_of_squares(number):
     return square_of_sum(number)-sum_of_squares(number)
