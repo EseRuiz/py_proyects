dbf35b7f62a87821741c68f97e668a41a403bfc2
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2024-04-29 19:07:41 -0500

    fix

--- proyects/clases/robot_name.py
+++ proyects/clases/robot_name.py
@@ -4,7 +4,6 @@ dictionary = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q
 
 class Robot:
     name = ""
-    name2 = ""
     def __init__(self):
         self.name = ''.join(random.choices(dictionary,k=2)).upper() + str(random.randrange(100,999))
 
