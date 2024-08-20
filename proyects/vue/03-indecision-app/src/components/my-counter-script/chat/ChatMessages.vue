0b1d98c75ca5d5a3536ee803aa17efc56cbee4d6
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2024-08-20 12:39:27 -0500

    Fix: <generando burbuja de mensaje y mensajes con gif>

--- proyects/vue/03-indecision-app/src/components/my-counter-script/chat/ChatMessages.vue
+++ proyects/vue/03-indecision-app/src/components/my-counter-script/chat/ChatMessages.vue
@@ -2,7 +2,8 @@
   <div class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
-      <ChatBubble></ChatBubble>
+      <ChatBubble :its-mine="true" :message="'Hola mundo'"></ChatBubble>
+      <ChatBubble :its-mine="false" :message="'no'" image="https://yesno.wtf/assets/no/29-6bf57c5bf3fed2dcdbed64afff7a7930.gif" ></ChatBubble>
     </div>
   </div>
 </template>
