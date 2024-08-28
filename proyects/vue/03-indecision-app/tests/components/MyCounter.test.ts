8939b6d6ae08e0417ab416643cb5cdb80e891c65
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2024-08-28 17:44:50 -0500

    Feat: <configuracion Vitest para localizacion de archivos y componentes>

---
+++ proyects/vue/03-indecision-app/tests/components/MyCounter.test.ts
@@ -0,0 +1,16 @@
+import { describe, test } from 'vitest';
+import { mount } from '@vue/test-utils';
+//import MyCounter from '../../../src/components/MyCounter.vue';
+import MyCounter from '@/components/MyCounter.vue';
+
+describe('<MyCounter>', () => {
+  test('should match snapshot', () => {
+    const wrapper = mount(MyCounter, {
+      props: {
+        value: 5
+      }
+    });
+
+    console.log(wrapper.html())
+  });
+});
