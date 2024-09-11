12a030a7b52bef2fbe07b353e7a306d0eca50710
EseRuiz <danielsruizovalle@yahoo.com>
2024-09-11 13:06:30 -0500

    Fix: <"creacion de modelos">

--- proyects/apis/api_listado_tareas/apilistado/taskcli/models.py
+++ proyects/apis/api_listado_tareas/apilistado/taskcli/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+import uuid
 
 class Task(models.Model):
+    """ 
+    Simple tracking of tasks
+    
+    """
+    id = models.UUIDField(
+        primary_key = True, 
+        default = uuid.uuid4,
+        editable = False, 
+        unique=True
+        )
     STATUS = { "T": "TODO" , "I" : "IN-PROGRESS" , "D" : "DONE"}
     task = models.CharField(max_length= 250, verbose_name= 'tarea')
     status = models.CharField(max_length= 1, choices= STATUS)
--- proyects/punto_net/holaMundo/HelloWord/obj/Debug/net8.0/HelloWord.AssemblyInfo.cs
+++ proyects/punto_net/holaMundo/HelloWord/obj/Debug/net8.0/HelloWord.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWord")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace13bfa979de227636567e71555da6a8263ea63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7dd0b3e9c90549525ca5098dbcf5879cb58db13")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWord")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWord")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
