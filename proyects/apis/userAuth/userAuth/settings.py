f465579186134195ff4f157aa2db182e8f0486ba
EseRuiz <danielsruizovalle@yahoo.com>
2024-11-06 19:17:21 -0500

    Fiz: <pruebas de funcionamiento>

--- proyects/apis/userAuth/userAuth/settings.py
+++ proyects/apis/userAuth/userAuth/settings.py
@@ -52,7 +52,7 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'user_auth.CustomUser'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=50),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
